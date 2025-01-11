14fa88c4372593e882f6ce47a5d6fc444d7bb25d
A-K-0802 <aalok.thakkar01@gmail.com>
2025-01-11 16:55:27 +0530

    Added Pages :)

--- lib/main.dart
+++ lib/main.dart
@@ -12,56 +12,78 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Firstpage());
+  }
+}
+
+class Firstpage extends StatefulWidget {
+  const Firstpage({super.key});
+
+  @override
+  State<Firstpage> createState() => _FirstpageState();
+}
+
+class _FirstpageState extends State<Firstpage> {
   String buttonname = 'Click';
   int current = 0;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My new app"),
-        ),
-        body: Center(
-            child: current == 0
-                ? Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    color: Colors.purple,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.amber,
-                            foregroundColor: Colors.white,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              buttonname = 'Clicked';
-                            });
-                          },
-                          child: Text(buttonname),
-                        ),
-                      ],
-                    ),
-                  )
-                : Image.asset('images/sp.jpg')),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings), label: 'Settings')
-          ],
-          currentIndex: current,
-          onTap: (int index) {
-            setState(() {
-              current = index;
-            });
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My new app"),
       ),
+      body: Center(
+          child: current == 0
+              ? Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Colors.purple,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.amber,
+                          foregroundColor: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) {
+                            return Second();
+                          }));
+                        },
+                        child: Text(buttonname),
+                      ),
+                    ],
+                  ),
+                )
+              : Image.asset('images/sp.jpg')),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
+        ],
+        currentIndex: current,
+        onTap: (int index) {
+          setState(() {
+            current = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class Second extends StatelessWidget {
+  const Second({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
