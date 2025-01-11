2d603739c50bb28ac845fbddb89ea5e9647d4c00
A-K-0802 <aalok.thakkar01@gmail.com>
2025-01-11 16:18:15 +0530

    Image using network

--- lib/main.dart
+++ lib/main.dart
@@ -23,44 +23,44 @@ class _MyAppState extends State<MyApp> {
           title: Text("My new app"),
         ),
         body: Center(
-          child: current == 0
-              ? Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.purple,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.amber,
-                          foregroundColor: Colors.white,
+            child: current == 0
+                ? Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    color: Colors.purple,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.amber,
+                            foregroundColor: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              buttonname = 'Clicked';
+                            });
+                          },
+                          child: Text(buttonname),
                         ),
-                        onPressed: () {
-                          setState(() {
-                            buttonname = 'Clicked';
-                          });
-                        },
-                        child: Text(buttonname),
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.amber,
-                          foregroundColor: Colors.white,
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.amber,
+                            foregroundColor: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              buttonname = 'Clicked';
+                            });
+                          },
+                          child: Text(buttonname),
                         ),
-                        onPressed: () {
-                          setState(() {
-                            buttonname = 'Clicked';
-                          });
-                        },
-                        child: Text(buttonname),
-                      ),
-                    ],
-                  ),
-                )
-              : SizedBox(),
-        ),
+                      ],
+                    ),
+                  )
+                : Image.network(
+                    'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2018/04/moon_imaged_by_pleiades_satellite/17482451-1-eng-GB/Moon_imaged_by_Pleiades_satellite_pillars.jpg')),
         bottomNavigationBar: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
